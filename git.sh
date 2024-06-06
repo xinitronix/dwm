facae60674794da0e4a52c8ebf875c7159fa602b
Your Name <you@example.com>
2024-06-07 00:18:53 +0300

    новый файл:    git.sh 	новый файл:    screen_check.sh 	новый файл:    xinitrc

---
+++ git.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+git add --all
+git add .
+git commit -n
+git push https://github.com/xinitronix/dwm.git
+
