146d85921807b6199c15c36735fe436ee887289e
Your Name <you@example.com>
2024-06-09 12:55:22 +0300

    modified:   install.sh 	new file:   pkg.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-!#/bin/sh
+#!/bin/sh
 cp wpa_supplicant.conf /etc
 cp screen_check.sh /usr/local/bin
 cp links+g         /usr/local/bin
 echo 'permit nopass keepenv definitly' >> /usr/local/etc/doas.conf
 su definitly -c 'cp xinitrc /home/definitly/.xinitrc'
-su definitly -c 'cp dwm_bar.sh /home/definitly
+su definitly -c 'cp dwm_bar.sh /home/definitly'
 su definitly -c 'cp Xdefaults /home/definitly/.Xdefaults'
---
+++ pkg.sh
@@ -0,0 +1,1 @@
+pkg install doas  htop  xterm dwm git-lite xfe yt-dlp mpv screen dmenu xsetroot mc links
