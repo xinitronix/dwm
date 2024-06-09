eae527fc19eb88ee3b0833c98e2d01ffa843192a
Your Name <you@example.com>
2024-06-09 08:59:13 +0000

    новый файл:    Xdefaults 	изменено:      dwm_bar.sh 	новый файл:    wifi.sh 	новый файл:    wpa_supplicant.conf 	изменено:      xinitrc

--- dwm_bar.sh
+++ dwm_bar.sh
@@ -7,10 +7,10 @@ do
 date=$( date +"%F %R" )
 tempcity=$(curl -s "https://api.openweathermap.org/data/2.5/weather?id=524894&appid=9cabf88b4c9c983d726a820860e3a5b6&units=metric" | jq -r '.main.temp')℃
 freemem=$(free | grep 'mem_free:' | awk '{print $5}' | sed 's/.$//') 
-tcpu=tCPU:$(sysctl -n dev.cpu.0.temperature | cut -c 1-2)℃
-topprocess=$(top -bn 1 | awk '{print $12}' | awk '(NR == 10)')
+tcpu=tCPU:$(sysctl -n hw.acpi.thermal.tz0.temperature | cut -c 1-4)℃
+topprocess=$(top -bn 1 | awk '{print $11}' | awk '(NR == 8)')
 loadcpu=cpu:$(top -bn 1 | grep "CPU:" | awk '{print $2 }' )
-loadtop=$(top -bn 1 | awk '{print $11}' | awk '(NR == 10)')
+loadtop=$(top -bn 1 | awk '{print $10}' | awk '(NR == 8)')
 skb=$(xset -q | awk 'BEGIN { a[1]="ru"; a[0]="en"; } /LED/ { print a[$10 && 32]; }')
 compat=$(sysctl -a | grep compat.linux.emul_path | awk '{print $2 }' | cut -c 9-)
 freessd=SSD:$(df -H / |  awk '{print $4 }'  | awk '(NR == 2)')
---
+++ wifi.sh
@@ -0,0 +1,4 @@
+doas killall -9 wpa_supplicant
+
+doas /etc/rc.d/wpa_supplicant start wlan0
+
